6e9ce20abe218e74918f07b3425b1f32fce52dd3
jonander <jonanderasua@gmail.com>
2022-05-09 12:24:44 +0200

    Arreglado lo de la eskaera, siguiente paso hacer bien que printee los predicados

--- public/js/db.js
+++ public/js/db.js
@@ -89,6 +89,30 @@ function getGuztia(){ //Aurreko metodoetan atera ez diren tripleak atera
 				<http://www.w3.org/2002/07/owl#inverseOf>))
 		}`
 
+	eskaera = `
+		PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+		select distinct ?s ?p ?o where { 
+			?s ?p ?o .
+			FILTER(?p  IN (<https://schema.org/parent>,
+						<https://schema.org/owns>,
+						<https://schema.org/spouse>,
+						<https://schema.org/knows>,
+						<https://schema.org/mentions>,
+						<https://schema.org/participant>,
+						<https://schema.org/author>,
+						<https://schema.org/worksFor>,
+						<http://ehu.eus/transparentrelations#pays>,
+						<http://ehu.eus/transparentrelations#registered_in>,
+						<http://ehu.eus/transparentrelations#beneficiary_of>,
+						<http://ehu.eus/transparentrelations#represents>,
+						<http://ehu.eus/transparentrelations#controls>,
+						<http://ehu.eus/transparentrelations#manages>,
+						<http://ehu.eus/transparentrelations#has_bank_account_in>,
+						<http://ehu.eus/transparentrelations#happens_in>))
+		}
+	`
+	
 	//Eskaeraren exekuzioa eta haren promesaren kudeaketa
 	return d3.sparql(uri,eskaera).then((data) => {
 		return data;
