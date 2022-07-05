ea2247a67f33a3ad5f7b138dc48b312441054c14
Jon Ander Asua <jasua@coninpe.es>
2022-07-05 11:08:30 +0200

    Sigo con lo de buscar

--- public/js/db.js
+++ public/js/db.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.skypack.dev/d3-sparql";
 
 //Metodoak exportatzeko
-export {getType,getLabel,getComment,getGuztia}
+export {getType,getLabel,getComment,getGuztia,getUriFromLabel}
 
 //Zein URIren kontra egingo diren eskaerak adierazi
 var uri = "http://jonander:7200/repositories/LaDonacion"
@@ -86,7 +86,37 @@ function getUriFromLabel(label){
 			return ''
 		}
 	})
-})
+}
+
+function getRelations(uriObj){
+	var eskaera = `
+	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+	select distinct ?s ?p ?o where { 
+		`+ uriObj +` ?p ?o .
+		FILTER(?p  IN (<https://schema.org/parent>,
+					<https://schema.org/owns>,
+					<https://schema.org/spouse>,
+					<https://schema.org/knows>,
+					<https://schema.org/author>,
+					<https://schema.org/worksFor>,
+					<http://ehu.eus/transparentrelations#pays>,
+					<http://ehu.eus/transparentrelations#registered_in>,
+					<http://ehu.eus/transparentrelations#beneficiary_of>,
+					<http://ehu.eus/transparentrelations#represents>,
+					<http://ehu.eus/transparentrelations#controls>,
+					<http://ehu.eus/transparentrelations#manages>,
+					<http://ehu.eus/transparentrelations#has_bank_account_in>,
+					<http://ehu.eus/transparentrelations#happens_in>,
+					<http://ehu.eus/transparentrelations#sibling>,
+					<http://ehu.eus/transparentrelations#related_to>))
+	}
+	`
+
+	d3.sparql(uri,eskaera).then((data) => {
+		
+	})
+}
 
 function getGuztia(){ //Aurreko metodoetan atera ez diren tripleak atera
 
